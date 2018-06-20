c7deea88e9b3351eef0bb331ac667cded42b7da8
gsiles54 <gabriel_siles@hotmail.com>
2018-06-20 17:12:22 -0300

    Octava iteracion

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,14 +1,12 @@
 package loom.hola_mundo;
-
 /**
 Clase Hola Mundo para Desafio Loom.
 @author Gabriel
- */
+*/
 public class HolaMundo {
-	/**
-	  Se encarga de concatenar el nombre a un saludo y devolver el String.
-	  @param persona
-	  @return saludo
+	/**Se encarga de concatenar el nombre a un saludo y devolver el String.
+	 * @param persona
+	 * @return saludo
 	 */
 	public String saludar(final String persona) {
 		StringBuilder saludo = new StringBuilder();
