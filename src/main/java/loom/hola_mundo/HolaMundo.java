75299b45218e02c0d36269f06ae65cb0adabe591
gsiles54 <gabriel_siles@hotmail.com>
2018-06-20 16:03:17 -0300

    Segunda iteracion

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -2,10 +2,7 @@ package loom.hola_mundo;
 
 public class HolaMundo {
 
-    public String saludar(final String persona) {
-    	
+    public String saludar(final String persona) {   	
     	return new StringBuilder().append("¡Hola, " ).append(persona).append("!").toString();
-    	
-       // throw new RuntimeException("¡Implementar este método!");
     }
 }
