2c0e52840049ec82552d499178eac02698b63e20
gsiles54 <gabriel_siles@hotmail.com>
2018-06-20 16:30:44 -0300

    Cuarta iteracion

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,9 +1,9 @@
 package loom.hola_mundo;
 
 public class HolaMundo {
-    public String saludar(final String persona) {   	
-    	StringBuilder saludo = new StringBuilder();
-    	saludo.append("¡Hola, " ).append(persona).append("!");
-    	return saludo.toString();
-    }
+	public String saludar(final String persona) {
+		StringBuilder saludo = new StringBuilder();
+		saludo.append("¡Hola, ").append(persona).append("!");
+		return saludo.toString();
+	}
 }
